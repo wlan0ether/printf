4a6e0cddf47adbcffe55137b65da9a65b4f7b3fa
NecheHephzibah <neche.bless@gmail.com>
2023-08-22 15:28:44 -0700

    Education is when you read the fine print. Experience is what you get if you don't

--- print_int.c
+++ print_int.c
@@ -9,28 +9,40 @@
  */
 void print_int(va_list param, int *calc)
 {
-	int num = va_arg(param, int);
-	int absolute_num = (num < 0) ? -num : num; /* Calculate absolute value*/
-	int divisor = 1;
+    int num = va_arg(param, int);
+    int num_copy = num;
+    int num_digits = 0;
+    int divisor = 1;
 
 	if (num < 0)
 	{
 		_putchar('-');
 		(*calc)++;
+		num = -num;
 	}
-
-	/* Calculate the divisor to get the leftmost digit*/
-	while (absolute_num / 10 != 0)
+	while (num_copy / 10 != 0)
 	{
+		num_digits++;
 		divisor *= 10;
-		absolute_num /= 10;
+		num_copy /= 10;
 	}
 
 	while (divisor > 0)
 	{
-		_putchar((absolute_num / divisor) + '0');
-		(*calc)++;
-		absolute_num %= divisor;
-		divisor /= 10;
+		if (num / divisor == -3)
+		{
+			_putchar('\t');
+		}
+		else if (num / divisor == -10)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar((num / divisor) + '0');
+			(*calc)++;
+			num %= divisor;
+			divisor /= 10;
+		}
 	}
 }
