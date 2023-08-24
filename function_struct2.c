f7957abb8f157b036c5fb8cc83d0e9ecefa534fc
wlan0ether <mwangijesse929@gmail.com>
2023-08-24 16:19:40 -0400

    perfecto tre

--- function_struct2.c
+++ function_struct2.c
@@ -50,4 +50,43 @@ int print_pointer(va_list param, char buffer[],
         	return (write_pointer(buffer, idx, length,
                     	wid, flag, filler, extra_c, filler_start));
 }
+/************************* PRINT NON PRINTABLE *************************/
+/**
+ * print_non_printable - print ascii codes in hex of non printable chars
+ * @param: container for list of arguments
+ * @buffer: Buffer array to handle print
+ * @flag:  Calculates active flags
+ * @wid: variable that get width
+ * @fmt_prec: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_non_printable(va_list param, char buffer[],
+        	int flag, int wid, int fmt_prec, int size)
+{
+        	int j = 0, offset = 0;
+        	char *str = va_arg(param, char *);
+ 
+        	UNUSED(flag);
+        	UNUSED(wid);
+        	UNUSED(fmt_prec);
+        	UNUSED(size);
+ 
+        	if (str == NULL)
+                    	return (write(1, "(null)", 6));
+ 
+        	while (str[j] != '\0')
+        	{
+                    	if (is_printable(str[j]))
+                                	buffer[j + offset] = str[j];
+                    	else
+                                	offset += append_hex_code(str[j], buffer, j + offset);
+ 
+                    	j++;
+        	}
+ 
+        	buffer[j + offset] = '\0';
+ 
+        	return (write(1, buffer, j + offset));
+}
 
