a3e17035c0e33e127385e930bc0782d0a6214a8f
wlan0ether <mwangijesse929@gmail.com>
2023-08-25 06:45:07 -0400

    making changes nine

--- converters.c
+++ converters.c
@@ -13,4 +13,26 @@ int is_printable(char c)
  
         	return (0);
 }
+/**
+ * append_hex_code – This inputs ascii in hexadecimal code to the buffer
+ * @buffer: contains an array of chars.
+ * @j: The Index where appending starts
+ * @ascii_code: This contains the standard ASCII codes.
+ * Return: returns 3 always
+ */
+int append_hex_code(char ascii_code, char buffer[], int j)
+{
+        	char map_to[] = "0123456789ABCDEF";
+        	/* The hex format code is always 2 digits long */
+        	if (ascii_code < 0)
+                    	ascii_code *= -1;
+ 
+        	buffer[i++] = '\\';
+        	buffer[i++] = 'x';
+ 
+        	buffer[i++] = map_to[ascii_code / 16];
+        	buffer[i] = map_to[ascii_code % 16];
+ 
+        	return (3);
+}
 
